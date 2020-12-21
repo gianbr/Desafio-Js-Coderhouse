51b370a25b81031a9ced4b3e18a65636bcff814c
Gian <gianlu_009@hotmail.com>
2020-12-20 22:51:08 -0300

    Orden de codigo

--- js/main.js
+++ js/main.js
@@ -20,10 +20,6 @@ window.addEventListener("scroll", function(){
 let enlacesHeader = document.querySelectorAll(".enlaces-header")[0];
 
 document.querySelectorAll(".hamburguer")[0].addEventListener("click", function(){
-    // let color = $(".hamburguer i").css("color")
-    // if((color == "black")){
-    //     $(".hamburguer i").css("color", "white")
-    // }
     enlacesHeader.classList.toggle("menudos")
 })
 
@@ -212,13 +208,13 @@ var shoppingCart = (function() {
       output += "<tr>"
         + "<td class='product-name'>" + cartArray[i].name + "</td>" 
         + "<td class='product-price'>$" + cartArray[i].price + "</td>"
-        + "<td><div class='input-group product-display'><button class='minus-item input-group-addon btn btn-primary' data-name=" + cartArray[i].name + ">-</button>"    // DAR ESTILO
-        + "<input type='number' class='item-count form-control product-input' data-name='" + cartArray[i].name + "' value='" + cartArray[i].count + "'>"              // *********************
-        + "<button class='plus-item btn btn-primary input-group-addon' data-name=" + cartArray[i].name + ">+</button></div></td>"                       // *********************
-        + "<td><button class='delete-item btn btn-danger' data-name=" + cartArray[i].name + ">X</button></td>"                                          // *********************
-        + " = "                                                                                                                                         // *********************
-        + "<td>$" + cartArray[i].total + "</td>"                                                                                                         // *********************
-        +  "</tr>";                                                                                                                                     // *********************
+        + "<td><div class='input-group product-display'><button class='minus-item input-group-addon btn btn-primary' data-name=" + cartArray[i].name + ">-</button>"    
+        + "<input type='number' class='item-count form-control product-input' data-name='" + cartArray[i].name + "' value='" + cartArray[i].count + "'>"              
+        + "<button class='plus-item btn btn-primary input-group-addon' data-name=" + cartArray[i].name + ">+</button></div></td>"                       
+        + "<td><button class='delete-item btn btn-danger' data-name=" + cartArray[i].name + ">X</button></td>"                                          
+        + " = "                                                                                                                                         
+        + "<td>$" + cartArray[i].total + "</td>"                                                                                                        
+        +  "</tr>";                                                                                                                                     
     }
     $('.show-cart').html(output);
     $('.total-cart').html(shoppingCart.totalCart());
